e5cc1e8652d0a25127f537a55254167f9ee7621f
Lorenz Walthert <lorenz.walthert@icloud.com>
2024-07-22 22:20:05 +0000

    version 0.4.3

--- tests/testthat/test-hook-lintr.R
+++ tests/testthat/test-hook-lintr.R
@@ -1,3 +1,5 @@
+# .R ----
+
 # success
 run_test("lintr",
   suffix = "-success.R",
@@ -7,8 +9,18 @@ run_test("lintr",
 # failure
 run_test("lintr", suffix = "-fail.R", std_err = "not lint free")
 
-# warning
-run_test(
-  "lintr",
-  suffix = "-fail.R", cmd_args = "--warn_only", std_err = NULL
-)
+if (!on_windows_on_cran()) {
+  # warning
+  run_test(
+    "lintr",
+    suffix = "-fail.R", cmd_args = "--warn_only", std_err = NULL
+  )
+
+  # .qmd ----
+
+  # success
+  run_test("lintr", suffix = "-success.qmd", std_err = NULL)
+
+  # failure
+  run_test("lintr", suffix = "-fail.qmd", std_err = "not lint free")
+}
